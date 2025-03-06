b7a3d94bb33da49d93f5ebd55afc5a2397b93fc6
cmwharff <115747231+cmwharff@users.noreply.github.com>
2025-03-06 18:43:51 -0500

    undo

--- svelte.config.js
+++ svelte.config.js
@@ -17,8 +17,7 @@ const config = {
         // See https://kit.svelte.dev/docs/adapters for more information about adapters.
         adapter: adapter(),
         paths: {
-            base: dev ? '' : process.env.BASE_PATH,
-			relative: false
+            base: dev ? '' : process.env.BASE_PATH
         },
     }
 };
