78e49b67667188bf89e0938c971bf83607532685
cmwharff <115747231+cmwharff@users.noreply.github.com>
2025-03-06 17:32:23 -0500

    boink

--- svelte.config.js
+++ svelte.config.js
@@ -4,22 +4,22 @@ import {vitePreprocess} from '@sveltejs/kit/vite';
 // noinspection JSUnresolvedReference
 const dev = process.argv.includes('dev');
 
+// noinspection JSUnresolvedReference
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+    // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+    // for more information about preprocessors
+    preprocess: vitePreprocess(),
 
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter(),
-		paths: {
-            base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+    kit: {
+        // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+        // If your environment is not supported or you settled on a specific environment, switch out the adapter.
+        // See https://kit.svelte.dev/docs/adapters for more information about adapters.
+        adapter: adapter(),
+        paths: {
+            base: dev ? '' : process.env.BASE_PATH,
         },
-	}
-	
+    }
 };
 
 export default config;
