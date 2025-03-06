8c1934b4072704bc482f305ba9b9fdc8f0cd15b8
cmwharff <115747231+cmwharff@users.noreply.github.com>
2025-03-06 17:06:31 -0500

    deploy fix 2?

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,10 @@
-<script>
+<script lang="ts">
     import Index from "$lib/Index.svelte";
 </script>
+
+<svelte:head>
+    <title>VELMA</title>
+</svelte:head>
 
 <div class="page">
     <Index></Index>
