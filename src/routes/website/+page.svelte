3ae34a1b377e2ae176d8c3c6f64330197482eccc
cmwharff <115747231+cmwharff@users.noreply.github.com>
2025-04-01 15:43:34 -0400

    fixed website link breakout

--- src/routes/website/+page.svelte
+++ src/routes/website/+page.svelte
@@ -1,5 +1,5 @@
 <div class="text">
-    <iframe src="https://enes100.umd.edu" title="tools" height="100%" width="90%" style="background-color: #121B21;"></iframe>
+    <iframe src="https://enes100.umd.edu" title="tools" height="100%" width="90%" style="background-color: #121B21;" sandbox="true allow-same-origin allow-scripts" ></iframe>
 </div>
 
 <style>
