3b1e9e2e0ff80186f237eb42224666ad6ac55c70
cmwharff <115747231+cmwharff@users.noreply.github.com>
2025-04-01 15:12:40 -0400

    docs fiddle

--- src/routes/docs/proj_details/t_ms5.svelte
+++ src/routes/docs/proj_details/t_ms5.svelte
@@ -1,5 +1,5 @@
 <iframe
-    src="https://docs.google.com/document/d/1WDpcE1_cV-WTmQJXmgwCa0t6FOCX9oWqs-iZKP_opxw/edit?usp=sharing"
+    src="https://docs.google.com/document/d/e/2PACX-1vSFLHpf8p2p0zhl4L2aKseYKU9o91be04eaQBqMURGEkOEwkBC1QqDe3Sgg2ovlxJfIMtZKckG7TKGE/pub?embedded=true"
     title="doc"
     style="width:100%; height:100%;"
 ></iframe>
