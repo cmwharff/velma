6e56749343751a9019285c6c2c48238b53a57ea6
cmwharff <115747231+cmwharff@users.noreply.github.com>
2025-04-08 14:48:40 -0400

    reverted fullscreen

--- src/routes/website/+page.svelte
+++ src/routes/website/+page.svelte
@@ -16,7 +16,11 @@
     }
     /* Inline | http://localhost:5173/website/ */
 
-    :global(body) {
+    .page {
+        /* display: space-between; */
+    }
+
+    body {
         /* height: 96vh; */
         /* border-top: 2vh solid rgba(255, 210, 0, 1.00); */
         /* border-bottom: 2vh solid rgba(255, 210, 0, 1.00); */
@@ -24,7 +28,7 @@
     }
 
     @media (min-width: 1001px) {
-        :global(.page) {
+        .page {
             height: 100vh;
         }
     }
@@ -32,21 +36,21 @@
     /* Inline | http://localhost:5173/website/ */
 
     @media (min-width: 900px) {
-        :global(.index.s-kB-HWnCjBMdc) {
+        .index.s-kB-HWnCjBMdc {
             position: absolute;
             top: 30vh;
             min-height: 240px;
         }
     }
 
-    :global(.directory.s-kB-HWnCjBMdc) {
+    .directory.s-kB-HWnCjBMdc {
         /* background-color: rgba(255, 255, 255, 0.2); */
         background-color: #121b21 !important;
     }
 
     /* Inline | http://localhost:5173/website/ */
 
-    :global(.text.s-n_WG2DzFuXSt) {
+    .text.s-n_WG2DzFuXSt {
         /* height: 90vh; */
         height: 100vh;
     }
