37ba6cd2b2886af0159912520c8939edf9103e3e
cmwharff <115747231+cmwharff@users.noreply.github.com>
2025-04-08 14:45:56 -0400

    made website fullscreen

--- src/routes/website/+page.svelte
+++ src/routes/website/+page.svelte
@@ -1,10 +1,53 @@
 <div class="text">
-    <iframe src="https://enes100.umd.edu" title="tools" height="100%" width="90%" style="background-color: #121B21;" sandbox="true allow-same-origin allow-scripts" ></iframe>
+    <iframe
+        src="https://enes100.umd.edu"
+        title="tools"
+        height="100%"
+        width="100%"
+        style="background-color: #121B21;"
+        sandbox="true allow-same-origin allow-scripts"
+    ></iframe>
 </div>
 
 <style>
     .text {
-        height: 90vh;
+        height: 100vh;
         margin: 0;
+    }
+    /* Inline | http://localhost:5173/website/ */
+
+    :global(body) {
+        /* height: 96vh; */
+        /* border-top: 2vh solid rgba(255, 210, 0, 1.00); */
+        /* border-bottom: 2vh solid rgba(255, 210, 0, 1.00); */
+        height: 100vh;
+    }
+
+    @media (min-width: 1001px) {
+        :global(.page) {
+            height: 100vh;
+        }
+    }
+
+    /* Inline | http://localhost:5173/website/ */
+
+    @media (min-width: 900px) {
+        :global(.index.s-kB-HWnCjBMdc) {
+            position: absolute;
+            top: 30vh;
+            min-height: 240px;
+        }
+    }
+
+    :global(.directory.s-kB-HWnCjBMdc) {
+        /* background-color: rgba(255, 255, 255, 0.2); */
+        background-color: #121b21 !important;
+    }
+
+    /* Inline | http://localhost:5173/website/ */
+
+    :global(.text.s-n_WG2DzFuXSt) {
+        /* height: 90vh; */
+        height: 100vh;
     }
 </style>
